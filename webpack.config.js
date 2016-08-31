a3b362799be23d3e017d4b220386926a1e64b24c
Robert Mruczek <rtmruczek@gmail.com>
2016-08-31 11:21:44 -0400

    add eslintrc

--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,53 @@
-var HtmlWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { join } = require('path');
 
 module.exports = {
-  context: __dirname + "/src",
+  context: join(__dirname, '/src'),
   entry: [
-        "./index.js"
-      ],
+    './index.js',
+  ],
 
   output: {
-    filename: "./index.js",
-    path: __dirname + "/dist"
+    filename: './index.js',
+    path: join(__dirname, '/dist'),
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: 'index.html',
       inject: 'body',
-      filename: 'index.html'
-    })
+      filename: 'index.html',
+    }),
   ],
   devtool: 'inline-source-map',
   module: {
+    preLoaders: [
+       { test: /\.js$/, loader: 'eslint-loader', exclude: /node_modules/ },
+    ],
     loaders: [{
-        test: /\.js?$/,
-        exclude: /node_modules/,
-        loader: 'babel',
-      },
-      {
-        test: /\.css?$/,
-        loader: 'style!css'
-      },
-      {
-        test   : /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9=&.]+)?$/,
-        loader : 'file-loader'
-      },
-      {
-        test   : /\.(png|jpg)$/,
-        loader : 'url-loader?limit=8192'
-      }
+      test: /\.js?$/,
+      exclude: /node_modules/,
+      loader: 'babel',
+    },
+    {
+      test: /\.css?$/,
+      loader: 'style!css',
+    },
+    {
+      test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9=&.]+)?$/,
+      loader: 'file-loader',
+    },
+    {
+      test: /\.(png|jpg)$/,
+      loader: 'url-loader?limit=8192',
+    },
     ],
   },
 
   /** NEEDED FOR ENZYME **/
   externals: {
-  'react/addons': true,
-  'react/lib/ExecutionEnvironment': true,
-  'react/lib/ReactContext': true
-  }
+    'react/addons': true,
+    'react/lib/ExecutionEnvironment': true,
+    'react/lib/ReactContext': true,
+  },
   /** NEEDED FOR ENZYME **/
-}
+};
