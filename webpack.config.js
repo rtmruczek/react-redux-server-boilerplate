c73a191935ad53875d4069aa048bec1f728bc877
Robert Mruczek <rtmruczek@gmail.com>
2016-10-11 21:08:17 -0400

    update react-hot-loader and move babel into package.json

--- webpack.config.js
+++ webpack.config.js
@@ -30,7 +30,7 @@ module.exports = {
     loaders: [{
       test: /\.js?$/,
       exclude: /node_modules/,
-      loaders: ['react-hot', 'babel'],
+      loaders: ['babel'],
     },
     {
       test: /\.css?$/,
